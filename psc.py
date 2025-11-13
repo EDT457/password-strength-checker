97728de97c683d8b49e074e59b1e73c33c60340e
EDT457 <tan.ethan.d@gmail.com>
2025-11-13 16:21:19 -0700

    v2, gui with input box, showhide button, and strength message

--- psc.py
+++ psc.py
@@ -1,3 +1,5 @@
+import tkinter
+
 
 def check_password(password):
     score = 0
@@ -13,5 +15,40 @@ def check_password(password):
             break
     return score
 
+def on_submit(rl):
+    strength = ["Weaker", "Weak", "Strong", "Very Strong"]
+    password = user_input.get()
+    res = check_password(password)
+    rl.config(text = strength[res])
 
-print("score: " + str(check_password(input("Input your password:"))))
+
+def toggle():
+    global password_visible
+    if password_visible:
+        password_visible = False
+        showhide.config(text = "Show")
+    else:
+        password_visible = True
+        showhide.config(text = "Hide")
+    user_input.config(show="•" if not password_visible else "")
+
+password_visible = False
+
+root = tkinter.Tk()
+root.title("Password Checker")
+root.geometry("400x300")
+
+tkinter.Label(root, text = "password").pack()
+
+user_input = tkinter.Entry(root, show="•")
+user_input.pack()
+
+showhide = tkinter.Button(root, text="Show", command= toggle)
+showhide.pack()
+
+result_label = tkinter.Label(root, text="")
+result_label.pack()
+
+tkinter.Button(root, text="Submit", command=lambda: on_submit(result_label)).pack()
+
+root.mainloop()
